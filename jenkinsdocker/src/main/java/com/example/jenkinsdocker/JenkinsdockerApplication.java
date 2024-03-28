d649bb1349d6ffe06f443bd8842cda4c3b17491c
sem <93970075+semtwo@users.noreply.github.com>
2024-03-28 10:28:24 +0900

    init project

---
+++ jenkinsdocker/src/main/java/com/example/jenkinsdocker/JenkinsdockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsdockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsdockerApplication.class, args);
+    }
+
+}
---
+++ jenkinsdocker/src/test/java/com/example/jenkinsdocker/JenkinsdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsdockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
